aef1e8d929619366ddc40e0d9475e4dd1401e3d2
shahbaz-alam7 <alamshahbaz287@gmail.com>
2022-11-07 11:10:11 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import jwt_decode from "jwt-decode";
+import "./App.css";
 
 function App() {
+  const [user, setUser] = useState({});
+  const handleCallbackResponsez = (response) => {
+    // console.log("token ", response.credential);
+    let userObject = jwt_decode(response.credential);
+    console.log(userObject);
+    setUser(userObject);
+    document.getElementById("signInDiv").hidden = true;
+  };
+  const signOutHandle = (e) => {
+    setUser({});
+    document.getElementById("signInDiv").hidden = false;
+  };
+  useEffect(() => {
+    /*global google*/
+    google.accounts.id.initialize({
+      client_id:
+        "685827706441-6gsel3e5uq4edb9qk9ui9nthf7bevjac.apps.googleusercontent.com",
+      callback: handleCallbackResponsez,
+    });
+    google.accounts.id.renderButton(document.getElementById("signInDiv"), {
+      theme: "outline",
+      size: "large",
+    });
+    google.accounts.id.prompt();
+  }, []);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div id="signInDiv"></div>
+      {user && (
+        <div>
+          <img src={user.picture} alt="" />
+          <p>{user.name}</p>
+          <p>{user.email}</p>
+        </div>
+      )}
+      {Object.keys(user).length != 0 && (
+        <button onClick={(e) => signOutHandle(e)}>Sign Out</button>
+      )}
     </div>
   );
 }
 
 export default App;
+// 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
+// 685827706441-6gsel3e5uq4edb9qk9ui9nthf7bevjac.apps.googleusercontent.com
